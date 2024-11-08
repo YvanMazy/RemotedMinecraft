89a5884d944e06616ac4deed004f6f158eaa94a2
YvanMazy <mazy.yvan@protonmail.com>
2024-11-08 22:28:08 +0100

    :sparkles: Add premain on example agent

--- example/src/main/java/be/yvanmazy/remotedminecraft/example/agent/AgentMain.java
+++ example/src/main/java/be/yvanmazy/remotedminecraft/example/agent/AgentMain.java
@@ -2,9 +2,14 @@ package be.yvanmazy.remotedminecraft.example.agent;
 
 import be.yvanmazy.remotedminecraft.controller.agent.RemotedAgents;
 
+import java.lang.instrument.Instrumentation;
 import java.rmi.RemoteException;
 
 public class AgentMain {
+
+    public static void premain(final String option, final Instrumentation inst) throws RemoteException {
+        agentmain(option);
+    }
 
     public static void agentmain(final String option) throws RemoteException {
         RemotedAgents.init(option, MyCustomAgent.ID, MyCustomAgentImpl::new);
